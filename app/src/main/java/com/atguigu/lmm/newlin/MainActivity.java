d07aeccc85ab118e53256ad732b91c4c9c814504
zpf1991 <404004281@qq.com>
2017-01-06 11:49:26 +0800

    11

---
+++ app/src/main/java/com/atguigu/lmm/newlin/MainActivity.java
@@ -0,0 +1,20 @@
+package com.atguigu.lmm.newlin;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.test.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        System.out.print("11111111");
+        System.out.print("11111111");
+        System.out.print("11111111");
+        System.out.print("11111111");
+    }
+}
